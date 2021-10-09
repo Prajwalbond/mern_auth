8825d42a27ef1c40103eb961f7756be6eb4009f2
Prajwalbond <prajwalbondarde0101@gmail.com>
2021-10-09 21:47:15 +0530

    modified userRouter.js

--- Server/routers/userRouter.js
+++ Server/routers/userRouter.js
@@ -1,7 +1,9 @@
  const router = require("express").Router();
  const User = require("../models/userModels");
  const bcrypt = require("bcryptjs");
+ const jwt = require("jsonwebtoken"); 
 
+ //register
  router.post("/", async(req, res) => {
     try{
      const {email,password,passwordVerify} = req.body;
@@ -32,7 +34,7 @@
        // password hashing
        const salt = await bcrypt.genSalt();
        const passwordHash = await bcrypt.hash(password, salt);
-       console.log(passwordHash);
+       //console.log(passwordHash);
 
        //save new user account to  db
        const newUser = new User({
@@ -41,11 +43,86 @@
 
        const savedUser = await newUser.save();
 
+       // sign the token
+       const token = jwt.sign({
+          user: savedUser._id
+       },process.env.JWT_SECRET);
+
+       console.log(token);
+
+       // send the token to HTTP-only cookie
+
+       res.cookie("token",token,{
+          httpOnly : true
+       })
+       .send();
+
+
     }
     catch (err) {
         console.error(err);
         res.status(500).send();
     }
- })   
+ });
+
+ // login
+
+ router.post("/login",async (req,res) => {
+    try{
+      const {email,password} = req.body;
+
+      //validate
+
+      if(!email || !password)
+      return res
+      .status(400)
+      .json({errorMessage: "Please enter all required fields"});
+
+      const existingUser = await User.findOne({email});
+      if(!existingUser){
+         return res.status(401).json({errorMessage: "Wrong email or password"});
+      }
+
+      const passwordCorrect = await bcrypt.compare(
+         password,
+         existingUser.passwordHash
+      );
+      if(!passwordCorrect){
+         return res.status(401).json({errorMessage: "Wrong email or password"});
+      }
+
+      // sign the token
+      const token = jwt.sign({
+         user: existingUser._id
+      },process.env.JWT_SECRET);
+
+      //console.log(token);
+
+      // send the token to HTTP-only cookie
+
+      res.cookie("token",token,{
+         httpOnly : true
+      })
+      .send();
+
+
+
+
+    }catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+ });
+
+ router.get("/logout", (req,res) => {
+    res
+      .cookie("token","", {
+      httpOnly : true,
+      expires : new Date(0),
+      })
+      .send();
+    
+ });
+
 
  module.exports = router;
